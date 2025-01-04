9c862393102a19c8a896cc6b3377ff33360c016e
Aid Puska <aidpuska29@gmail.com>
2025-01-04 13:10:24 +0100

    Log

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,5 @@
 export async function GET(request) {
+  console.log("request headers", request)
   console.log("REQUEST aid: ", request.headers.get('x-forwarded-for'))
   const ip = request.headers.get('x-forwarded-for')
   return Response.json({ ip })
